7820d61fe059183131af70d5b60ea895c4c8b297
alexotsu <alexotsu@gmail.com>
2017-11-13 20:26:33 +0700

    Added test for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -6,9 +6,19 @@ const {app} = require('./../server');
 // syntax above: ./ gets current directory, ../ moves one directory up
 const {Todo} = require('./../models/todo');
 
-// beforeEach
+
+// need to make dummy todos here because the below beforeEach resets the database to zero (for somereason)
+const todos = [{
+  text: 'First test todo'
+}, {
+  text: 'Second test todo'
+  }];
+
+// beforeEach is supertest syntax to run before each thing in the describe block
 beforeEach((done) => {
-  Todo.remove({}).then(() => done());
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -28,7 +38,8 @@ describe('POST /todos', () => {
           return done(err);
         }
 
-        Todo.find().then((todos) => {
+        Todo.find({text}).then((todos) => {
+          // use .find({text}) to find only the database entries with that text in it. This length should still be 1.
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done()
@@ -47,9 +58,22 @@ describe('POST /todos', () => {
         }
 
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0);
+          // change todos.length to expect 2, since
+          expect(todos.length).toBe(2);
           done();
         }).catch((e) => done(e));
       });
+  });
+});
+
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
   });
 });
